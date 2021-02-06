2995916ac3bf2331ba2dbc632565e37a0a27fed9
Jo Mendes <jlmendes1996@gmail.com>
2021-02-06 12:03:57 -0500

    task 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(function() {
   $(".carousel").carousel( { interval: 2000 } );
-  $("#carouselButton").click(function(){
+  $("#carouselButton").on("click", function(){
       if ($("#carouselButton").children("i").hasClass("fa-pause")) {
           $(".carousel").carousel("pause");
           $("#carouselButton").children("i").removeClass("fa-pause");
@@ -11,6 +11,15 @@ $(function() {
           $("#carouselButton").children("i").addClass("fa-pause"); 
       }
   });
+
+
+
+    $("#reserveButton").on("click", function() {
+        $("#reserveModal").modal("show");
+    ;})
+
+
+    $("#loginButton").on("click", function() {
+        $("#loginModal").modal("show");
+    ;})
 });
-
-
